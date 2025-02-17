f5d6f6d5e43ee639db13641b4fdc276bbbda16c5
Meet Shah <meet1993shah@gmail.com>
2025-02-17 12:32:06 -0500

    debug false

--- main.py
+++ main.py
@@ -91,4 +91,4 @@ if __name__ == '__main__':
     if platform.system() == 'Android':
         from android.permissions import Permission, request_permissions
         request_permissions([Permission.INTERNET, Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
-    app.run(debug=True, port=8080)
+    app.run(debug=False, port=8080)
